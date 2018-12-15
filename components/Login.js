9393efc8fc1bc57af87ccbc006d9510b7e3a665a
EpicSeven <realepicseven@users.noreply.github.com>
2018-12-15 20:53:04 +0100

    smelost

--- components/Login.js
+++ components/Login.js
@@ -1,24 +1,41 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet,Text,View,StatusBar,Image, TouchableNativeFeedback, TextInput,SafeAreaView,Keyboard, TouchableOpacity,KeyboardAvoidingView} from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  StatusBar,
+  TextInput,
+  SafeAreaView,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView
+} from "react-native";
 
 export default class Login extends Component {
-    render (){
-       return(
-       <View style={styles.containers}>
-            <StatusBar 
-            barStyle= 'light-content'
-            backgroundColor= '#3E637B'> </StatusBar>
-        </View>
-        )
-
-    }
-
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 1, justifyContent: "flex-start" }} />
+        <Image source={require("../image/logo.png")} />
+      </View>
+    );
+  }
 }
-const styles = StyleSheet.create ({
-    container: {
-        backgroundColor: '#3E637B',
-        flex: 1,
-        flexDirection: 'column',
-    }
-
-})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgb(32, 53, 70)",
+    flex: 1,
+    flexDirection: "column"
+  },
+  logoContainer: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: "center"
+  },
+  logo: {
+    height: 100,
+    width: 100
+  }
+});
